d81aa5d21b78577a58efa230cd2d6f2daa9d50ef
shaheryarbhatti <shaheryar_b@yahoo.com>
2026-06-04 07:38:40 +0500

    design change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>VexSonic – Innovative Software Solutions</title>
   <meta name="description" content="VexSonic delivers world-class Website Development, Mobile Apps, and SAP ABAP solutions." />
+  <link rel="icon" type="image/png" href="{{ asset('images/vexsonicfavicon.png') }}" />
+  <link rel="shortcut icon" type="image/png" href="{{ asset('images/vexsonicfavicon.png') }}" />
+  <link rel="apple-touch-icon" href="{{ asset('images/vexsonicfavicon.png') }}" />
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Space+Grotesk:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="{{ asset('css/landing.css') }}" />
 </head>
