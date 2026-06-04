aa2af4756719612d799ee7e89de6d21bb91fb493
shaheryarbhatti <shaheryar_b@yahoo.com>
2026-06-04 12:57:34 +0500

    design change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,15 @@
   <link rel="apple-touch-icon" href="{{ asset('images/vexsonicfavicon.png') }}" />
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Space+Grotesk:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="{{ asset('css/landing.css') }}" />
+  <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXET1DYLT9"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-XXET1DYLT9');
+    </script>
 </head>
 <body>
 
